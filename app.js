3c84fe644cb345f6bc42e9a67c38fa2248b5ca77
Kevin Cruz <kevin_cruzz@outlook.es>
2022-03-15 00:48:49 -0600

    Add 4 basic operations functionality + clear screen

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const screenValue = document.querySelector('.screen');
+const screenValue = document.querySelector('.interactive-zone');
+const resultValue = document.querySelector('.results')
 init();
 
 function init() {
@@ -17,18 +18,31 @@ let operation = "", number1 = "", number2 = "";
 
 function appBrain(value) {
     if (value === "+" || value === "-" || value === "x" || value === "/") {
+
         if (operation !== "") return;
         screenValue.textContent += ` ${value} `
         operation = value;
+
     } else if (value === "AC") {
         cleanScreen();
     } else if (value === "Erase") {
         updateDisplay();
     } else if (value === "=") {
+
+        parseInt(number1, 10);
+        parseInt(number2, 10);
         if (operation === "+" && number1 !== "" && number2 !== "") {
-            add(parseInt(number1, 10), parseInt(number2, 10));
+            add(number1, number2);
+        } else if (operation === "-" && number1 !== "" && number2 !== "") {
+            substract(number1, number2);
+        } else if (operation === "x" && number1 !== "" && number2 !== "") {
+            multiply(number1, number2);
+        } else {
+            divide(number1, number2);
         }
+
     } else {
+
         if (operation === "") {
             if (screenValue.textContent !== "0") {
                 screenValue.textContent += `${value}`;
@@ -41,23 +55,38 @@ function appBrain(value) {
             screenValue.textContent += `${value}`
             number2 += value;
         }
+
     }
 }
 
 function add(digit1, digit2) {
-    let sum = digit1 + digit2;
-    console.log(sum);
+    let op = digit1 + digit2;
+    resultValue.textContent = `${op}`;
     return; 
 }
 
 function substract(digit1, digit2) {
-    return digit1 - digit2;
+    let op = digit1 - digit2;
+    resultValue.textContent = `${op}`;
+    return; 
 }
 
 function multiply(digit1, digit2) {
-    return digit1 * digit2;
+    let op = digit1 * digit2;
+    resultValue.textContent = `${op}`;
+    return; 
 }
 
 function divide(digit1, digit2) {
-    return digit1 / digit2;
+    let op = digit1 / digit2;
+    resultValue.textContent = `${op}`;
+    return; 
+}
+
+function cleanScreen() {
+    screenValue.textContent = "0";
+    resultValue.textContent = "";
+    operation = "";
+    number1 = ""; 
+    number2 = "";
 }
